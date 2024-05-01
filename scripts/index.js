0481b28d2e4a0d2227ac3e04b3a1395b93ef24e2
ahadhalloul <ahadhalloul@hotmail.com>
2024-05-01 15:28:27 +0300

    add dashboard screen & collapsed sidebar

--- scripts/index.js
+++ scripts/index.js
@@ -116,3 +116,19 @@ window.addEventListener("click", function (e) {
     menuIcon.classList.remove("hide");
   }
 });
+
+//////////////////////////////////////////
+// sidebar menu toggle
+const sidebar = document.getElementById("sidebar-open");
+const collapsedSidebar = document.getElementById("sidebar-collapsed");
+// const openSidebarIcon = document.getElementById("sidebar-collapsed-icon");
+// const collapseSidebarIcon = document.getElementById("sidebar-open-icon");
+function collapseSidebar() {
+  sidebar.classList.add("hide");
+  collapsedSidebar.classList.remove("hide");
+}
+
+function openSidebar() {
+  sidebar.classList.remove("hide");
+  collapsedSidebar.classList.add("hide");
+}
